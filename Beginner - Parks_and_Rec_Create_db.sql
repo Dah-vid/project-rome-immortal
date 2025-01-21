487feba588aa5e36a583dc302165eaffce7e3535
David Okunfolami <34836799+Dah-vid@users.noreply.github.com>
2025-01-21 23:55:39 +0000

    Update Beginner - Parks_and_Rec_Create_db.sql
    Updated

--- Beginner - Parks_and_Rec_Create_db.sql
+++ Beginner - Parks_and_Rec_Create_db.sql
@@ -1,4 +1,6 @@
 SELECT *
 FROM employee_demographics
 #WHERE (birth_date > "1980-01-01" AND gender = "male") OR gender = "female";
-WHERE (first_name = "Leslie" AND age = 44) OR age > 44
+#WHERE (first_name = "Leslie" AND age = 44) OR age > 44
+
+where first_name LIKE "Jer%"
