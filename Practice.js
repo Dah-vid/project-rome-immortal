8b97308dbccd19784c01eb6f1699aa28a0fc8b7f
David Okunfolami <34836799+Dah-vid@users.noreply.github.com>
2025-01-27 21:27:40 +0000

    Updated Practice.JS

--- Practice.js
+++ Practice.js
@@ -1,1 +1,12 @@
 #This is a test folder
+
+let sum = (a, b) => a + b;
+
+/* This arrow function is a shorter form of:
+
+let sum = function(a, b) {
+  return a + b;
+};
+*/
+
+alert( sum(1, 2) ); // 3
